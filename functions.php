2c62f463afccefc20042f1da435be5d226e422bf
Asaad Khattab <asaad1592@gmail.com>
2018-04-07 15:38:29 -0400

    added create and update for login php

---
+++ functions.php
@@ -0,0 +1,36 @@
+<?php include "db.php";?>
+<?php
+  function showAllData() {
+    global $connection;
+    $query = "SELECT * FROM users";
+    $result = mysqli_query($connection, $query);
+    if(!$result) {
+      die('Query not working' . mysqli_error());
+    }
+
+
+    while($row = mysqli_fetch_assoc($result)) {
+      $id = $row['id'];
+      echo "<option value='$id'>$id</option>";
+    }
+  }
+
+
+function UpdateTable() {
+
+  global $connection;
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $id = $_POST['id'];
+
+  $query = "UPDATE users SET "
+  $query .= "username = '$username', ";
+  $query .= "password = '$password' ";
+  $query .= "WHERE id = $id ";
+
+    $result = mysqli_query($connection, $query);
+
+    if(!$result){
+      die("Query failed" . mysqli_error($connection));
+    }
+}
