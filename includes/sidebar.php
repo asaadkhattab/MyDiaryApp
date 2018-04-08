710bdba20e5d4ffdf640935674dc9ea6f8ed0680
Asaad Khattab <asaad1592@gmail.com>
2018-04-08 18:40:16 -0400

    made a search engine

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,18 +1,28 @@
+<!-- Blog Sidebar Widgets Column -->
 <div class="col-md-4">
+
+
 
     <!-- Blog Search Well -->
     <div class="well">
-        <h4>Blog Search</h4>
+        <h4>Diary Search</h4>
+        <form class="" action="includes/search.php" method="post">
         <div class="input-group">
-            <input type="text" class="form-control">
+            <input name="search" type="text" class="form-control">
             <span class="input-group-btn">
-                <button class="btn btn-default" type="button">
+                <button name="submit" class="btn btn-default" type="submit">
                     <span class="glyphicon glyphicon-search"></span>
             </button>
             </span>
         </div>
+        </form>
         <!-- /.input-group -->
     </div>
+
+
+
+
+
 
     <!-- Blog Categories Well -->
     <div class="well">
--- index.php
+++ index.php
@@ -17,11 +17,11 @@
                 $select_all_posts = mysqli_query($connection, $query);
 
                 while($row = mysqli_fetch_assoc($select_all_posts)){
-                  $post_title = $row['post_title'];
-                  $post_author = $row['post_author'];
+                  $post_title  = $row['post_title'];
+                  $post_author  = $row['post_author'];
                   $post_date    = $row['post_date'];
                   $post_image   = $row['post_image'];
-                  $post_content = $row['post_content']
+                  $post_content = $row['post_content'];
                   ?>
 
                   <h1 class="page-header">
@@ -38,7 +38,7 @@
                   </p>
                   <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
                   <hr>
-                  <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                  <img class="img-responsive" src="images/<<?php echo $post_image; ?>" alt="">
                   <hr>
                   <p><?php $post_content ?></p>
                   <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
@@ -62,8 +62,8 @@
 
             </div>
 
-<!-- Blog Sidebar Widgets Column -->
-<<?php include "includes/sidebar.php" ?>
+
+<?php include "includes/sidebar.php" ?>
 
         </div>
         <!-- /.row -->
