4f453a9bd3228919ca3beb968808aba206812f29
Asaad Khattab <asaad1592@gmail.com>
2018-04-07 20:50:16 -0400

    refactored crud

--- functions.php
+++ functions.php
@@ -1,5 +1,48 @@
 <?php include "db.php";?>
 <?php
+
+  function createRows() {
+    global $connection;
+    if(isset($_POST['submit'])) {
+
+      $username = $_POST['username'];
+      $password = $_POST['password'];
+
+
+      $query = "INSERT INTO users(username, password) ";
+      $query .= "VALUE ('$username ', '$password')";
+
+      $result = mysqli_query($connection, $query);
+
+      if(!$result) {
+        die('Query not working' . mysqli_error());
+      } else
+      {
+        echo "User Created.";
+      }
+
+      if($username && $password){
+        echo $username;
+        echo $password;
+      } else {
+        echo "Make sure you set the fields!";
+      }
+    }
+  }
+
+  function readRows(){
+    global $connection;
+    $query = "SELECT * FROM users ";
+    $result = mysqli_query($connection, $query);
+    if (!$result) {
+      die('query failed ' . mysqli_error())
+    }
+
+    while($row = mysqli_fetch_assoc($result)){
+      print_r($row);
+    }
+  }
+
   function showAllData() {
     global $connection;
     $query = "SELECT * FROM users";
@@ -17,7 +60,7 @@
 
 
 function UpdateTable() {
-
+if(isset($_POST['submit'])){
   global $connection;
   $username = $_POST['username'];
   $password = $_POST['password'];
@@ -30,13 +73,18 @@ function UpdateTable() {
 
     $result = mysqli_query($connection, $query);
 
-    if(!$result){
+    if(!$result) {
       die("Query failed" . mysqli_error($connection));
+    } else
+    {
+      echo "Updated Successfully!"
     }
+  }
 }
 
 function deleteRows() {
 
+if(isset($_POST['Submit'])) {
   global $connection;
   $username = $_POST['username'];
   $password = $_POST['password'];
@@ -50,5 +98,8 @@ function deleteRows() {
 
     if(!$result){
       die("Query failed" . mysqli_error($connection));
+    } else {
+      echo "Deleted!"
     }
+  }
 }
--- login_create.php
+++ login_create.php
@@ -1,45 +1,13 @@
-<?php include "db.php";
+<?php include "db.php"; ?>
+<?php include "funcitons.php"; ?>
+<?php include "includes/header.php"; ?>
 
-if(isset($_POST['submit'])) {
-
-  $username = $_POST['username'];
-  $password = $_POST['password'];
-
-
-  $query = "INSERT INTO users(username, password) ";
-  $query .= "VALUE ('$username ', '$password')";
-
-  $result = mysqli_query($connection, $query);
-
-  if(!$result) {
-    die('Query not working' . mysqli_error());
-  }
-
-  if($username && $password){
-    echo $username;
-    echo $password;
-  } else {
-    echo "Make sure you set the fields!";
-  }
-}
-
+<?php
+  createRows();
 ?>
 
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
+    <h1>Create</h1>
 
     <div class="container">
       <div class="col-sm-6">
@@ -56,17 +24,7 @@ if(isset($_POST['submit'])) {
           <div class="form-group">
             <input class="form-group" type="password" name="password" value="">
           </div>
-          <input class="btn btn-primary" type="submit" name="submit" value="Submit">
+          <input class="btn btn-primary" type="submit" name="submit" value="Add User">
         </form>
       </div>
-    </div>
-
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
-</html>
+<?php include "includes/footer.php" ?>
